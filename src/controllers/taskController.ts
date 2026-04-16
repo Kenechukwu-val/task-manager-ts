81e648c5533fd4c52a0a2068c589c3e2ee18130c
Kenechukwu Nwafor <kenechukwunwafor@Kenechukwus-MacBook-Pro.local>
2026-04-16 18:05:10 +0100

    Modified the taskController

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -11,4 +11,60 @@ export const getTasks = async (req: Request, res: Response) => {
     }catch ( error: any ) {
         res.status(500).json({ success: false, message: error.message });
     }
+};
+
+//Create a new task
+export const createTask = async (req: Request, res: Response) => {
+    try{
+        const task = new Task({
+            ...req.body,
+            user: req.userId
+        });
+        await task.save();
+
+        res.status(201).json({ success: true, task });
+    } catch ( error: any) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+};
+
+//Update a task
+export const updateTask = async ( req: Request, res: Response ) => {
+    try{
+        const task = await Task.findOneAndUpdate({
+                _id: req.params.id,
+                user: req.userId
+        },
+        req.body,
+        { new: true, runValidators: true }
+    );
+
+        if ( !task ) {
+            return res.status(404).json({ success: false, message: 'Task not found' });
+        }
+
+        res.json({ success: true, task });
+
+    } catch (error: any) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+};
+
+//Delete a task
+export const deleteTask = async ( req: Request, res: Response ) => {
+    try{
+        const task = await Task.findOneAndDelete({
+            _id: req.params.id,
+            user: req.userId
+        });
+    
+        if ( !task ) {
+            return res.status(404).json({ success: false, message: 'Task not found' });
+        }
+        res.json({ success: true, message: 'Task deleted successfully' });
+        
+    } catch (error: any) {
+            res.status(500).json({ success: false, message: error.message });
+    }
 }
+    
