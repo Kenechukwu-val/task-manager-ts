73b70b69d3e01941173004632527dbc85a40ff73
Kenechukwu Nwafor <kenechukwunwafor@Kenechukwus-MacBook-Pro.local>
2026-04-17 16:22:05 +0100

    Modified the server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,23 @@ import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import http from 'http';
 
 import authRoutes from './routes/authRoutes';
 import taskRoutes from './routes/taskRoutes';
+import { setupSocket } from './sockets/socketHandler';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Create HTTP server ( needed for Socket.IO to work with Express )
+const httpServer = http.createServer(app);
+
+const io = setupSocket(httpServer); // Initialize Socket.IO with the HTTP server
+
+app.set('io', io); // Make Socket.IO instance available in routes/controllers
 
 // Middleware
 app.use(helmet()); // Security headers
@@ -35,6 +44,7 @@ mongoose.connect(process.env.MONGO_URI as string)
 
 
 // Start the server
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
+    console.log(`🔌 Socket.io is ready for real-time connections`);
 })
